a1438a2e02b8af3696f40566c4644823234fc509
CyberDagger <cyberdagger003@gmail.com>
2025-07-03 17:35:54 +0100

    Fixed top navigation bar
    + rough menu item function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./reset.css";
 import "./styles.css";
 import home from "./home.js";
 import menu from "./menu.js";
--- src/menu.js
+++ src/menu.js
@@ -1,21 +1,26 @@
 export default function write() {
     content.innerHTML = "";
 
-    const pizza = document.createElement("div");
-    pizza.classList.add("menu-item");
+    function makeItem() {
+        const pizza = document.createElement("div");
+        pizza.classList.add("menu-item");
 
-    const pizzaImage = document.createElement("img");
-    pizzaImage.setAttribute("src", "https://placehold.co/400");
+        const pizzaImage = document.createElement("img");
+        pizzaImage.setAttribute("src", "https://placehold.co/400");
 
-    const pizzaName = document.createElement("h2");
-    pizzaName.textContent ="Paimon Pepperoni";
+        const pizzaName = document.createElement("h2");
+        pizzaName.textContent ="Paimon Pepperoni";
 
-    const pizzaDesc = document.createElement("p");
-    pizzaDesc.textContent = "Hottest slices you've ever seen.";
+        const pizzaDesc = document.createElement("p");
+        pizzaDesc.textContent = "Hottest slices you've ever seen.";
 
-    pizza.appendChild(pizzaImage);
-    pizza.appendChild(pizzaName);
-    pizza.appendChild(pizzaDesc);
+        pizza.appendChild(pizzaImage);
+        pizza.appendChild(pizzaName);
+        pizza.appendChild(pizzaDesc);
 
-    content.appendChild(pizza);
+        content.appendChild(pizza);
+    }
+
+    makeItem();
+    makeItem();
 }
