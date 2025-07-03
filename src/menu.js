4c624023646372b004f4cabc7ddc1f9ea2fef86b
CyberDagger <cyberdagger003@gmail.com>
2025-07-03 13:34:57 +0100

    Menu item structure

--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,21 @@
 export default function write() {
     content.innerHTML = "";
 
-    const headline = document.createElement("h1");
-    headline.textContent = "Paimon Pepperoni";
+    const pizza = document.createElement("div");
+    pizza.classList.add("menu-item");
 
-    const blurb = document.createElement("p");
-    blurb.textContent = "Hottest slices you've ever seen.";
+    const pizzaImage = document.createElement("img");
+    pizzaImage.setAttribute("src", "https://placehold.co/400");
 
-    content.appendChild(headline);
-    content.appendChild(blurb);
+    const pizzaName = document.createElement("h2");
+    pizzaName.textContent ="Paimon Pepperoni";
+
+    const pizzaDesc = document.createElement("p");
+    pizzaDesc.textContent = "Hottest slices you've ever seen.";
+
+    pizza.appendChild(pizzaImage);
+    pizza.appendChild(pizzaName);
+    pizza.appendChild(pizzaDesc);
+
+    content.appendChild(pizza);
 }
